706194e2b69c2d93a8174f56468b25e7dce04f7f
IrinaBreiksh <irinabreiksh@gmail.com>
2022-09-20 19:02:18 +0300

    Corrected mistakes

--- main.js
+++ main.js
@@ -4,28 +4,23 @@ console.log(user);
 const student = 'Irina';
 console.log(student);
 
-user = {
-  student,
-};
-
-/**
- * выведется Irina
- */
-console.log(user);
+user = student;
+// will be displayed Irina
+console.log(user); // displayed Irina
 
 let test = 1;
 test += 1;
-test + '1';
-// раз есть кавычки, то должна быть  строка '21'
-console.log(test); // но вывелось 2
+test += '1';
+// should be string '21'
+console.log(test); // we got '21'
 
 test -= 1;
-// будет 1
-console.log(test); // вывелось 1
+// will be displayed 1
+console.log(test); // displayed 1
 
-Boolean(test);
-// будет 1
-console.log(test); // вывелось 1
+test = Boolean(test);
+// should be 1
+console.log(test); // displayed 1
 
 const array = [2, 3, 5, 8];
 console.log(array);
@@ -38,12 +33,10 @@ console.log(result);
 
 const numbers = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > 5 && numbers[i] < 10)
-    console.log(numbers[i]);
+  if (numbers[i] > 5 && numbers[i] < 10) console.log(numbers[i]);
 }
 
 const numbers1 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < numbers1.length; i++) {
-  if (numbers1[i] % 2 === 0 && numbers1 !== 0)
-    console.log(numbers1[i]);
+  if (numbers1[i] % 2 === 0 && numbers1[i] !== 0) console.log(numbers1[i]);
 }
