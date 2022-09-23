f03072df1d7077c90532ea82d75509e8b33d9e0b
IrinaBreiksh <irinabreiksh@gmail.com>
2022-09-23 10:13:09 +0300

    Made function min/max, palindromeword, 0/zero

--- main.js
+++ main.js
@@ -40,3 +40,64 @@ const numbers1 = [2, 5, 8, 15, 0, 6, 20, 3];
 for (let i = 0; i < numbers1.length; i++) {
   if (numbers1[i] % 2 === 0 && numbers1[i] !== 0) console.log(numbers1[i]);
 }
+
+function min1(a, b) {
+  if (a < b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+console.log(min1(300, 93));
+
+function max1(a, y) {
+  if (a > y) {
+    return a;
+  } else {
+    return y;
+  }
+}
+console.log(max1(5, 10));
+
+
+function min2(a, b) {
+  return a < b ? a : b;
+}
+min2(222, 4);
+
+function max2(a, b) {
+  return a > b ? a : b;
+}
+max2(222, 4);
+
+
+const min = (a, b) => a < b;
+min(222, 4);
+const max = (a, b) => a < b;
+max(333,52);
+
+const Numbers = [5, 30, 8, 78, 60, 44, 3, 19, 20, 88];
+function renameZero() {
+  for (let i = 0; i < Numbers.length; i++) {
+    if (Numbers[i] % 10 === 0) {
+      console.log(`${Numbers[i]}zero`);
+    } else {
+      console.log(Numbers[i]);
+    }
+  }
+return Numbers;
+}
+renameZero();
+console.log(Numbers);
+
+function word() {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[word.length - i]) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(word('mom'));
+console.log(word('one'));
+
