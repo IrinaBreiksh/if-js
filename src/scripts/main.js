e58689485c20fc6671c4e0c95874884392ecc2c3
IrinaBreiksh <irinabreiksh@gmail.com>
2022-11-18 10:15:08 +0300

    Added in function some parameters, added test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,4 @@
-export const palindrome = (word) => {
-  return word === word.split('').reverse().join('');
-};
-
-console.log(palindrome('dfggfd'));
-
-const obj1 = {
+const object1 = {
   a: 'a',
   b: {
     a: 'a',
@@ -14,7 +8,7 @@ const obj1 = {
     },
   },
 };
-const obj2 = {
+const object2 = {
   b: {
     c: {
       a: 1,
@@ -24,7 +18,7 @@ const obj2 = {
   },
   a: 'a',
 };
-const obj3 = {
+const object3 = {
   a: {
     c: {
       a: 'a',
@@ -35,17 +29,28 @@ const obj3 = {
   b: 'b',
 };
 
-const deepEqual = () => {
-  const values1 = Object.getOwnPropertyNames(obj1);
-  const values2 = Object.getOwnPropertyNames(obj2);
 
-  for (let i = 0; i < values1.length; i++) {
-    if (obj1[values1[i]] !== obj2[values2[i]]) {
+export function deepEqual(object1, object2) {
+  const value1 = Object.getOwnPropertyNames(object1);
+  const value2 = Object.getOwnPropertyNames(object2);
+
+  if (value1.length !== value2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < value1.length; i += 1) {
+    const values = value1[i];
+    const bothAreObjects = typeof(object1[values]) === 'object' && typeof(object2[values]) === 'object';
+
+    if ((!bothAreObjects && (object1[values] !== object2[values]))
+      || (bothAreObjects && !deepEqual(object1[values], object2[values]))) {
       return false;
     }
   }
-  return true;
-};
 
-console.log(deepEqual(obj1, obj2));
-console.log(deepEqual(obj1, obj3));
+  return true;
+}
+
+console.log(deepEqual(object1, object2));
+console.log(deepEqual(object1, object3));
+
